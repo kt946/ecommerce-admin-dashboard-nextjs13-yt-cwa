cb403ecad34842dc2d8a66d02001b5cb2fa6ee82
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-25 15:16:44 -0400

    Updated layout of sign-in and sign-up forms

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs';
 
-export default function Home() {
+const SetupPage = () => {
   return (
-    <div className='p-4'>
-      <Button>Click Me</Button>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
-}
+  );
+};
+
+export default SetupPage;
