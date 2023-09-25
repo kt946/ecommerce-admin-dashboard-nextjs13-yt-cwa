bdff36bc60a3f1e196f3dfd133f00ee93769a7f4
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-25 14:45:37 -0400

    Updated app layout and added Button component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='p-4'>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
