8c03ea76ff493940fd2c632a993884fee5fd3652
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-26 17:27:34 -0400

    Added navbar component

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,8 @@
-import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
+
+import prismadb from '@/lib/prismadb';
+import Navbar from '@/components/navbar';
 
 export default async function DashboardLayout({
   children,
@@ -28,7 +30,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This will be a Navbar</div>
+      <Navbar />
       {children}
     </>
   );
