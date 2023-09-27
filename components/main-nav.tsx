13920bb720ff561a825262dd75686d503fe436fd
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-27 17:06:39 -0400

    Added Billboards form page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,6 +15,11 @@ export function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElemen
       active: pathname === `/${params.storeId}`,
     },
     {
+      href: `/${params.storeId}/billboards`,
+      label: 'Billboards',
+      active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: 'Settings',
       active: pathname === `/${params.storeId}/settings`,
