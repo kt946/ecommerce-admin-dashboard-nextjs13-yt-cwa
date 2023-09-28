dc6ab48f2836e0d676660b9185b21452ab1df0c2
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-28 13:50:54 -0400

    Added billboard Get, Patch, and Delete routes

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -60,6 +60,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
         await axios.post(`/api/${params.storeId}/billboards`, data);
       }
       router.refresh();
+      router.push(`/${params.storeId}/billboards`);
       toast.success(toastMessage);
     } catch (error: any) {
       toast.error('Something went wrong.');
--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -50,3 +50,22 @@ export async function POST(req: Request, { params }: { params: { storeId: string
     return new NextResponse('Internal error', { status: 500 });
   }
 }
+
+export async function GET(req: Request, { params }: { params: { storeId: string } }) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse('Store id is required', { status: 400 });
+    }
+
+    const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboards);
+  } catch (error) {
+    console.log('[BILLBOARDS_GET]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
