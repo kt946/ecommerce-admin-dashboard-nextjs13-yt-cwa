8a91d438b967375e2a7440cee1df69c0b2ed7728
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-28 15:16:35 -0400

    Added API list for billboards client

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,8 +6,10 @@ import { useParams, useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { BillboardColumn, columns } from './columns';
 import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
+
+import { BillboardColumn, columns } from './columns';
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -34,6 +36,15 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
         searchKey="label"
         columns={columns}
         data={data}
+      />
+      <Heading
+        title="API"
+        description="API calls for Billboards"
+      />
+      <Separator />
+      <ApiList
+        entityName="billboards"
+        entityIdName="billboardId"
       />
     </>
   );
