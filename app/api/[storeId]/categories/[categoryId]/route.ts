90208dd76133fa6b1bec098d4c540899603aee77
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-30 15:36:27 -0400

    Added Stripe package and webhook

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -13,6 +13,9 @@ export async function GET(req: Request, { params }: { params: { categoryId: stri
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
 
     return NextResponse.json(category);
