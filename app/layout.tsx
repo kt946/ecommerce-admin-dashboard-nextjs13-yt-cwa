900942da775d86fbd1c4d78d7854269984f767b9
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-30 16:22:56 -0400

    Added dark mode toggle button

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 
 // local imports
-import { ModalProvider } from '@/providers/modal-provider';
 import { ToastProvider } from '@/providers/toast-provider';
+import { ModalProvider } from '@/providers/modal-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
 
 // relative imports
 import './globals.css';
@@ -22,9 +23,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToastProvider />
-          <ModalProvider />
-          {children}</body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
